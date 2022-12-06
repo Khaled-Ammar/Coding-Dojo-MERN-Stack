338415470a9c95a63f82494b1ca8f4eebdcf8432
Khaled-Ammar <111436094+Khaled-Ammar@users.noreply.github.com>
2022-12-06 14:55:38 +0200

    react

---
+++ REACT/firstjsx/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {    
+  render(){
+  return ( <div>
+    <h1>Hello Dojo</h1>  
+  <p>Things I need to do</p> 
+  <ul>
+    <li>Learn React </li>
+    <li> Clmb Mt Everest </li>
+    <li>run</li>
+    <li>Feed dojs</li>
+  </ul>
+
+  </div> );    
+  }
+  }
+  
+
+export default App;
