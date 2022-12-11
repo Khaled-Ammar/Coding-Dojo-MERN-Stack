4d49406b6b77ed003894843d6dd5b2efc07d79da
Khaled-Ammar <111436094+Khaled-Ammar@users.noreply.github.com>
2022-12-11 14:19:30 +0200

    boxs

---
+++ REACT/colorb/ox/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react'
+import Displays from './components/Display';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <Displays/>
+
+    </div>
+
+    </>
+  );
+}
+
+export default App;
---
+++ REACT/colorb/ox/src/components/Display.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+
+const Displays = (props) => {
+    const [color,setColor]=useState("");
+    const [box,setBox]=useState([]);
+
+
+    const sumbitHandle = (e)=>{
+        e.preventDefault()
+        setColor(color)
+        setBox((box)=>[...box,color])
+    }
+    const styles= {
+        display: 'flex',
+        justifyContent: 'space-evenly',
+
+    }
+  return (
+    
+    <div>
+
+      <form onSubmit={sumbitHandle}>
+        <label>Enter The Color</label>
+        <input
+                    onChange={(e) => (setColor(e.target.value))}
+                ></input>
+        <input type="submit" value="Add" />
+      </form>
+      <br></br>
+      <div style={{
+                    display: 'flex',
+                    justifyContent: 'space-evenly',
+                }}>
+      {box.map((item, i) =>
+                <div >
+                    <div style={{
+                        width: `100px`,
+                        height: `100px`,
+                        border: '2px solid black',
+                        backgroundColor: `${item}`,
+                        justifyContent: 'center',
+                        marginTop: '20px',
+                    }} key={i}>{item}
+                    </div>
+                   
+
+                 
+                              </div>
+                              )}
+    </div>
+    </div>
+  )
+}
+
+export default Displays
